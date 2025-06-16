fdf1b998302872178e9939dfff58f5e45d701cdd
Aayush <aayushjha2431@gmail.com>
2025-06-16 20:06:06 +0530

    creating the new file

---
+++ OOP.py
@@ -0,0 +1,12 @@
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+    def circumference(self):
+        return 2 * 3.14 * self.radius
+
+c1 = Circle(7)
+print(c1.area())
